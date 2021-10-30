4aeb39488f06150baef8693859d84a7aa529db3e
p.menegatti <p.menegatti@omia.fr>
2021-10-30 14:10:20 +0200

    gan loss added again (problem with git)

--- train.py
+++ train.py
@@ -16,6 +16,7 @@ device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cp
 # Dataset: https://www.kaggle.com/c/gan-getting-started/data
 monet_path_train = "dataset/train/monet/"
 photo_path_train = "dataset/train/photos/"
+photo_path_test = "dataset/test/photos/"
 monet_dataset = ImageDataset(monet_path_train)
 photo_dataset = ImageDataset(photo_path_train)
 photo_dataset_test = ImageDataset(photo_path_test)
@@ -33,11 +34,11 @@ photo_dataloader = torch.utils.data.DataLoader(photo_dataset,
 
 
 # Load model
-# TODO
-F = Generator() # photo generator
-G = Generator() # monet generator
-D_monet = Discriminator() # 1 real, 0 fake
-D_photo = Discriminator()
+
+F = Generator().to(device) # photo generator
+G = Generator().to(device) # monet generator
+D_monet = Discriminator().to(device) # 1 real, 0 fake
+D_photo = Discriminator().to(device)
 
 opt_F = torch.optim.SGD(lr=lr, params=F.parameters(), momentum=momentum)
 opt_G = torch.optim.SGD(lr=lr, params=G.parameters(), momentum=momentum)
@@ -78,22 +79,49 @@ def train_one_epoch(epoch, F, G, D_photo, D_monet, photo_dl, monet_dl):
         reconstructed_monet = G(fake_photo)
 
         # Cycle loss
-        cycleloss_FG = cycle_loss(reconstructed_photo, photo)
-        cycleloss_GF = cycle_loss(reconstructed_monet, monet)
+        cycleloss_FG = cycle_loss(photo, reconstructed_photo)
+        cycleloss_GF = cycle_loss(monet, reconstructed_monet)
 
+        # GAN loss generator
+        gloss_monet_fake = gan_loss(D_monet(fake_monet), torch.FloatTensor([[1]]).to(device))
+        floss_photo_fake = gan_loss(D_photo(fake_photo), torch.FloatTensor([[1]]).to(device))
+
+        gan_generator_losses = gloss_monet_fake.sum() + floss_photo_fake.sum()
+
+        opt_Dm.zero_grad()
+        opt_Dp.zero_grad()
+
+        print(D_photo(photo))
         # GAN loss discriminator
         # D_photo vs F
-        dloss_photo_real = gan_loss(D_photo(photo), 1)
-        dloss_photo_fake = gan_loss(D_photo(fake_photo), 0)
+        dloss_photo_real = gan_loss(D_photo(photo), torch.FloatTensor([[1]]).to(device))
+        dloss_photo_fake = gan_loss(D_photo(fake_photo.detach()), torch.FloatTensor([[0]]).to(device))
         # D_monet vs G
-        dloss_monet_real = gan_loss(D_monet(monet), 1)
-        dloss_monet_fake = gan_loss(D_monet(fake_monet), 0)
+        dloss_monet_real = gan_loss(D_monet(monet), torch.FloatTensor([[1]]).to(device))
+        dloss_monet_fake = gan_loss(D_monet(fake_monet.detach()), torch.FloatTensor([[0]]).to(device))
 
         # total loss and backpropagation
         l = 10
-        total_loss = l * (cycleloss_FG + cycleloss_GF) + dloss_photo_real + dloss_photo_fake + dloss_monet_real + dloss_monet_fake
-
+        dloss_photo = (dloss_photo_real + dloss_photo_fake).sum()
+        dloss_monet = (dloss_monet_real + dloss_monet_fake).sum()
+        cycle_losses = (cycleloss_FG + cycleloss_GF).sum()
+        total_loss = l * cycle_losses + dloss_photo + dloss_monet + gan_generator_losses
         total_loss.backward()
+
+
+        opt_Dm.step()
+        opt_Dp.step()
+        opt_F.step()
+        opt_G.step()
+
+
+        print("total loss is ", total_loss.item())
+        #print("cycle loss is ", cycle_losses.item())
+        #print("d photo loss is ", dloss_photo.item())
+        #print("d monet loss is ", dloss_monet.item())
+
+
+
 
         #Tensorboard
         writer.add_scalar("cycle loss",
@@ -140,6 +168,38 @@ def train_one_epoch(epoch, F, G, D_photo, D_monet, photo_dl, monet_dl):
 
 
 
+photo_sampler = torch.utils.data.RandomSampler(photo_dataset, replacement=False)
+monet_sampler = torch.utils.data.RandomSampler(monet_dataset, replacement=False)
+#Start tensor board
+import datetime
+import os
+
+# Create directories for logs
+log_dir = "runs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+summary_dir = log_dir + "/summary"
+models_dir = log_dir + "/models"
+test_dir = log_dir + "/test"
+os.mkdir(log_dir)
+os.mkdir(summary_dir)
+os.mkdir(models_dir)
+os.mkdir(test_dir)
+
+# Start tensorboard
+#type "tensorboard --logdir=runs" in terminal
+writer = SummaryWriter(summary_dir)
+
+def test_one_epoch(F, epoch):
+    im_ten = photo_dataset_test[0].unsqueeze(0).to(device)
+    with torch.no_grad():
+        new_im = F(im_ten)
+        torchvision.utils.save_image(new_im.cpu(), f"{test_dir}/epoch{epoch:03d}.png")
+
 for epoch in tqdm(range(num_epochs)):
-    train_one_epoch(epoch, F, G, D_photo, D_monet, photo_dataloader, monet_dataloader)
+    print("Epoch:", epoch)
+    torch.save(F.state_dict(), models_dir + "/F.pth")
+    torch.save(G.state_dict(), models_dir + "/G.pth")
+    torch.save(D_monet.state_dict(), models_dir + "/D_monet.pth")
+    torch.save(D_photo.state_dict(), models_dir + "/D_photo.pth")
+    train_one_epoch(epoch, F, G, D_photo, D_monet, photo_dataset, monet_dataset)
+    test_one_epoch(F, epoch)
 
